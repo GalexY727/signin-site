c4c9f30dc19b6fcdcf81627719cd976c5ef9b6b5
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-12-20 17:12:45 -0800

    Establish a nice background and prepare for a table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="login">
+        <h1>Sign in</h1>
+          <form method="post">
+            <input type="text" name="u" placeholder="Enter your name" required="required" />
+          </form>
+      </div>
+      <div class="names">
+        <table class="table">
+          <tr>
+            <td>Aleg</td>
+            <td>Aleg</td>
+            <td>Aleg</td>
+            <td>Aleg</td>
+          </tr>
+          <tr>
+            <td>Jacueblol</td>
+            <td>Jacueblol</td>
+            <td>Jacueblol</td>
+            <td>Jacueblol</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
