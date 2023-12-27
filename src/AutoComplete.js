793a89be8d7423e961f8dda38c6bdc0fa718a280
Alexander Hamilton <a.hamilton72006@gmail.com>
2023-12-26 22:49:45 -0800

    cleanup autocomplete.js and make it not be based upon useeffect (only update it when necesary)
    Co-authored-by: Keigo Morita <Kekechi@users.noreply.github.com>

--- src/AutoComplete.js
+++ src/AutoComplete.js
@@ -4,31 +4,36 @@ import './AutoComplete.css';
 const AutoComplete = ({ whitelist , onSubmit }) => {
     const [value, setValue] = useState("");
     const [showSuggestions, setShowSuggestions] = useState(false);
+    const [suggestions, setSuggestions] = useState([]);
     const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
 
-    const suggestions = value.match(/[a-z0-9]/i) 
-        ? whitelist
-            .filter((name) => name.toLowerCase().indexOf(value.toLowerCase()) > -1)
-            .slice(0, 5) // Only take the first three suggestions
-        : [];
-
-    const autoCompleteHandler = value + (suggestions[activeSuggestionIndex] || '').slice(value.length);
+    const [autoCompleteValue, setRealAutoCompleteValue] = useState("");
     
+    const setAutoCompleteValue = (name, index = activeSuggestionIndex, filter = suggestions) => {
+        let ghostText =
+          name !== ""
+            ? name +
+              (filter[index] || "").slice(name.length)
+            : "";
+        setRealAutoCompleteValue(ghostText);
+    }
+
     const keyDownHandler = (e) => {
         switch (e.key) {
             case 'ArrowDown':
                 const newIndexDown = activeSuggestionIndex + 1 < suggestions.length ? activeSuggestionIndex + 1 : 0;
                 setActiveSuggestionIndex(newIndexDown);
+                setAutoCompleteValue(value, newIndexDown);
                 e.preventDefault();
                 break;
             case 'ArrowUp':
                 const newIndexUp = activeSuggestionIndex - 1 >= 0 ? activeSuggestionIndex - 1 : suggestions.length - 1;
                 setActiveSuggestionIndex(newIndexUp);
+                setAutoCompleteValue(value, newIndexUp);
                 e.preventDefault();
                 break;
             case 'Tab':
             case 'Enter':
-                e.preventDefault();
                 setValue(suggestions[activeSuggestionIndex] || '');
                 setShowSuggestions(false);
                 // Send the ref and give the code time to access
@@ -42,23 +47,49 @@ const AutoComplete = ({ whitelist , onSubmit }) => {
                 // so that it can be sent to onSubmit
                 setTimeout(() => {
                     setValue('');
+                    setAutoCompleteValue('');
                 }, 10);
                 setShowSuggestions(false);
                 e.preventDefault();
                 break;
-            case 'Backspace':
-                if (e.ctrlKey) {
-                    const words = value.trim().split(/\s+/);
-                    words.pop();
-                    setValue(words.join(' '));
-                    e.preventDefault();
-                }
-                break;
             default:
-            if (!showSuggestions)
-                setActiveSuggestionIndex(0);
-                setShowSuggestions(true);
         }
+    }
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+        const filter = e.target.value.match(/[a-z0-9]/i)
+          ? whitelist
+              .filter(
+                (name) =>
+                  name.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
+              )
+              .slice(0, 5) // Only take the first three suggestions
+          : [];
+          
+        setSuggestions(filter.slice(0, 5));
+        if (filter.length > 0) {
+            setShowSuggestions(true);
+            setActiveSuggestionIndex(0);
+            setAutoCompleteValue(e.target.value, 0, filter);
+        } else {
+            setShowSuggestions(false);
+            setAutoCompleteValue('');
+        }
+    }
+
+    const onClickHandler = (e) => {
+        setValue(e.target.innerText);
+        setShowSuggestions(false);
+        // Send the ref and give the code time to access
+        // the value since it was just updated
+        setTimeout(() => {
+            onSubmit(autoCompleteRef);
+            setTimeout(() => {
+                setValue('');
+                setAutoCompleteValue('');
+            }, 10);
+        }, 10);
     }
 
     useEffect(() => {
@@ -74,30 +105,23 @@ const AutoComplete = ({ whitelist , onSubmit }) => {
         };
     }, []);
 
-    useEffect(() => {
-        if (suggestions.length > 0) {
-            setShowSuggestions(true);
-        } else {
-            setShowSuggestions(false);
-        }
-    }, [suggestions]);
-
     const autoCompleteRef = useRef();
 
     return (
         <div className="auto-complete">
-            <div className="form" style={{ position: 'relative' }}>
-                <input
+            <div style={{ position: 'relative' }}>
+                <input  
                     className="auto-complete-ghost"
                     type="text"
-                    value={autoCompleteHandler}
+                    value={autoCompleteValue}
+                    placeholder=""
                     disabled
                 />
                 <input
                     style={{ position: 'relative', zIndex: 2 }}
                     type="text"
                     value={value}
-                    onChange={(e) => setValue(e.target.value)}
+                    onChange={onChange}
                     ref={autoCompleteRef}
                     onKeyDown={keyDownHandler}
                     placeholder="Enter your full name"
@@ -109,7 +133,7 @@ const AutoComplete = ({ whitelist , onSubmit }) => {
             {showSuggestions && (
                 <ul className="suggestions">
                     {suggestions.map((suggestion, index) => (
-                        <li className={index === activeSuggestionIndex ? 'active' : ''} key={suggestion} onClick={() => setValue(suggestion)}>
+                        <li className={index === activeSuggestionIndex ? 'active' : ''} key={suggestion} onClick={onClickHandler}>
                             {suggestion}
                         </li>
                     ))}
