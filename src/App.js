1ee91c3d7d1d30d5cbbd3f640ffbde95e1067e5c
Jacob Hotz <77470805+Jacob1010-h@users.noreply.github.com>
2024-01-06 01:47:26 -0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import Home from "./pages/Home";
 import Dev from "./pages/Dev";
+import LoginPage from "./pages/LoginPage"
 
 import {Route, Routes, BrowserRouter} from "react-router-dom";
+import {useState} from "react";
 
 function App() {
+
+    const [appState, setAppState] = useState('login');
+
+    const onLogin = function(key) {
+        if (key === process.env.REACT_APP_LOGIN_PASSWORD)
+            setAppState('chat');
+    };
+
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/Dev" element={<Dev/>}/>
-                    <Route path="*" element={<h1>404</h1>}/>
-                </Routes>
-            </BrowserRouter>
+            {
+                appState === 'login' ?
+                    <LoginPage onLogin={onLogin}/> :
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/Dev" element={<Dev/>}/>
+                            <Route path="*" element={<h1>404</h1>}/>
+                        </Routes>
+                    </BrowserRouter>
+            }
         </div>
     );
 }
