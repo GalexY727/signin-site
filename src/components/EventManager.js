9e92c6a0661f2dbdd85704a29044cceddf68a063
Jacob Hotz <lordgaming1010@gmail.com>
2024-02-08 20:43:30 -0800

    You can now add durations to the names! - ALEX DID THIS JACOB CANT DO NOTHING :D
    Co-authored-by: rin-star <rin-star@users.noreply.github.com>
    Co-authored-by: Alexander Hamilton <a.hamilton72006@gmail.com>

--- src/components/EventManager.js
+++ src/components/EventManager.js
@@ -1,7 +1,6 @@
-import { InputGroup } from "react-bootstrap";
-import Form from "react-bootstrap/Form";
 import "./EventManager.css";
-import { useEffect, useReducer, useState } from "react";
+import "./ErrorList.css"
+import { useEffect, useState } from "react";
 
 function EventManager(input) {
     const [events, setEvents] = useState([]);
@@ -12,44 +11,8 @@ function EventManager(input) {
             minute: "2-digit",
         })
     );
-        
-    const dateObject = new Date(input.date + ' ' + time);
-
-    const handleSelectChange = (e) => {
-        setSignedInState(e.target.value === "In");
-    };
     
-
-    const handleTimeChange = (e) => {
-        setTime(e.target.value);
-    };
-
-    const convertTime12to24 = (time12h) => {
-        // check if time is already in 24hour format
-        if ((!time12h.includes("AM") && !time12h.includes("PM"))) {
-            return time12h;
-        }
-
-        const [time, modifier] = time12h.split(" ");
-
-        let [hours, minutes] = time.split(":");
-
-        if (hours === "12") {
-            hours = "00";
-        }
-
-        if (modifier === "PM") {
-            hours = parseInt(hours, 10) + 12;
-        }
-
-        return `${hours}:${minutes}`;
-    };
-
-    function toTitleCase(str) {
-        return str.replace(/\w\S*/g, function (txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-    }
+    const dateObject = new Date(input.date + ' ' + time);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -67,106 +30,71 @@ function EventManager(input) {
         fetchData();
     }, [input.name, input.date]);
 
-    const addNewEvent = () => {
-        if (input.name === "") {
-            alert("Please enter a name");
-            return;
+
+    const keyDownHandler = (e) => {
+        const value = e.target.value;
+        switch (e.key) {
+            case 'Tab':
+            case 'Enter':
+                if (value === '') {
+                    e.preventDefault();
+                    return;
+                }
+                const inputValue = value.trim();
+                // Make sure all of the data (trimmed) are numbers
+                if (!isNaN(inputValue)) {
+                    input.onSubmit(inputValue);
+                }
+
+            case 'Escape':
+                // in case the above case was just triggered,
+                // wait a bit before clearing the value
+                // so that it can be sent to onSubmit
+                e.target.value = "";
+                e.preventDefault();
+                break;
+            default:
         }
-        makeData(
-            toTitleCase(input.name),
-            signedInState ? "In" : "Out",
-            dateObject.toLocaleString([], {timeZone: "America/Los_Angeles"}).replace(",", "")
-        );
-    };
-
-    const makeData = async (name, inOrOut, timestamp) => {
-        const data = [
-            ["name", name],
-            ["timestamp", timestamp],
-            ["inOrOut", inOrOut],
-            ["studentOrParent", "Student"],
-        ];
-        postData(data);
-    };
-
-    const postData = (data) => {
-        var formDataObject = new FormData();
-
-        data.forEach((element) => {
-            formDataObject.append(element[0], element[1]);
-        });
-
-        let url = process.env.REACT_APP_SHEET_POST_URL;
-        fetch(url, { method: "POST", body: formDataObject }).catch((err) =>
-            console.log(err)
-        );
-    };
-
-    const removeEvent = (e) => {
-        
-        let index = events.indexOf(e.target.value);
-        makeData(
-            toTitleCase(input.name),
-            "Out",
-            input.date + " " + convertTime12to24(time) + ":01"
-        );
-
-        // let newEvents = [...events];
-        // newEvents.splice(index, 1);
-        // setEvents(newEvents);
-
-    };
+    }
 
     return (
         <div className="event-manager">
             <h1 style={{ color: "lightgray", textAlign:"center", paddingBottom:"0.5em"}}>Events: </h1>
-
-            <button onClick={addNewEvent} className="submit-task" />
-            <div style={{ overflow: "hidden" }}>
-                <InputGroup style={{ placeItems: "flex-end" }}>
-                    <Form.Select
-                        onChange={handleSelectChange}
-                        style={{ maxWidth: "30%" }}
-                    >
-                        <option>In</option>
-                        <option>Out</option>
-                    </Form.Select>
-
-                    <Form.Control type="time" onChange={handleTimeChange} />
-                </InputGroup>
-            </div>
-            
+            <input type="text" onKeyDown={keyDownHandler} />
             <hr className="solid" />
-
+            
             <div
                 className="events"
                 style={{ maxHeight: "calc(85vh - 19em)", overflow: "auto" }}
             >
                 {events && events.map((event, index) => (
-                    event &&
-                    <div className="event" key={index}>
-                        <button className="delete-task" onClick={removeEvent} />
-                        <div style={{ overflow: "hidden" }}>
-                            <InputGroup
-                                className="mb-3"
-                                style={{ placeItems: "flex-end" }}
-                            >
-                                <Form.Select
-                                    defaultValue={event.state}
-                                    style={{ maxWidth: "30%" }}
-                                >
-                                    <option>In</option>
-                                    <option>Out</option>
-                                </Form.Select>
-                                
-                                <Form.Control
-                                    defaultValue={event.time}
-                                    type="time"
-                                    readOnly
-                                />
-                            </InputGroup>
+                    event && (
+                        <div className="event" key={index}>
+                            
+                            <div style={{ overflow: "hidden" }}>
+                                <div className="mb-3" style={{ placeItems: "flex-end" }}>
+                                    <div style={{ display: "flex", width: "100%" }}>
+                                        <div style={{ width: "25%" }}>
+                                            <input
+                                                style={{ verticalAlign: "middle", width: "100%" }}
+                                                type="text"
+                                                value={event.state}
+                                                disabled
+                                            />
+                                        </div>
+                                        <div style={{ width: "75%" }}>
+                                            <input
+                                                defaultValue={event.time}
+                                                type="time"
+                                                readOnly
+                                                style={{ width: "100%" }}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    )
                 ))}
             </div>
         </div>
--- src/pages/Dev.js
+++ src/pages/Dev.js
@@ -5,6 +5,8 @@ import EventManager from "../components/EventManager";
 import ErrorList from "../components/ErrorList";
 import "./Dev.css"; 
 import { useSearchParams } from "react-router-dom";
+import { db } from "../utils/firebaseConfig.js";
+import { get, onValue, ref, set } from "firebase/database";
 
 function Dev() {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -44,6 +46,96 @@ function Dev() {
         // only get the month and day of the date
         setCurrentDate(e.target.value);
     };
+
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+
+    const handleDurationChange = (addedDuration) => {
+        // Get the current duration of the current day
+        // += the added duration
+        // Set the new duration
+        const data = getData();
+        
+        data.then((data) => {
+            let studentData = true ? data.Students : data.Parents;
+            try {
+                let duration = "0:0:0";
+                let titleCaseName = toTitleCase(searchName);
+                let [currentYear, currentMonth, currentDay] = currentDate.split("-");
+                if (currentDay.startsWith("0")) {
+                    currentDay = currentDay.slice(1);
+                }
+                if (currentMonth.startsWith("0")) {
+                    currentMonth = currentMonth.slice(1);
+                }
+                if (
+                    studentData[titleCaseName] &&
+                    studentData[titleCaseName][currentYear] &&
+                    studentData[titleCaseName][currentYear][currentMonth] &&
+                    studentData[titleCaseName][currentYear][currentMonth][currentDay]
+                ) {
+                    duration = studentData[titleCaseName][currentYear][currentMonth][currentDay].duration || "0:0:0";
+                    console.log(duration);
+                }
+                let [hours, minutes, seconds] = duration.split(":");
+                // the addedDuration is in hours
+                hours = parseInt(hours) + parseInt(addedDuration);
+                // clamp hours from 0-12
+                hours = Math.min(12, Math.max(0, hours));
+                duration = `${hours}:${minutes}:${seconds}`;
+
+                let [year, month, day] = urlDate.split("-");
+
+                // if the day starts with 0, remove it
+                if (day.startsWith("0")) {
+                    day = day.slice(1);
+                }
+                if (month.startsWith("0")) {
+                    month = month.slice(1);
+                }
+
+                setData(true, toTitleCase(urlName), year, month, day, null, duration);
+            }
+            catch (e) {
+                console.error(e);
+            }
+        });
+    }
+
+    const getData = () => {
+        return get(ref(db, '/')).then((snapshot) => {
+            if (snapshot.exists()) {
+                return snapshot.val();
+            }
+        }).catch((error) => {
+            console.error(error);
+        }).then((data) => {
+            return data;
+        });
+    }
+
+    const setData = async (isStudent = true, name, year, month, day, index, value) => {
+        // Adjust the path based on whether it's a student or not
+        const basePath = isStudent ? '/Students' : '/Parents';
+        
+        let path = `${basePath}/${name}/${year}/${month}/${day}`;
+
+        console.log(index);
+        if (index != null || index != undefined) {
+            console.log(index);
+            path += `/${index}`;
+            console.log(path);
+        }
+
+        path += `/${"duration"}`;
+        
+        console.log(path);
+        // Now, set the data using the modified path
+        set(ref(db, path), value);
+    }
 
     useEffect(() => {
         setSearchParams({ date: currentDate, name: searchName });
@@ -174,7 +266,6 @@ function Dev() {
     }, []);
 
     const getInitVal = () => {
-        // console.log(studentName);
         return (
             (searchName
                 ? searchName.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
@@ -189,6 +280,7 @@ function Dev() {
             <span className="grid-container">
                 <div className="left-col">
                     <div className="student-date">
+
                         <span>
                             <h1 style={{ color: "lightgray" }}>Student</h1>
                             <AutoComplete
@@ -212,6 +304,7 @@ function Dev() {
                         name={searchName}
                         date={currentDate.toString()}
                         getEventData={getEventData}
+                        onSubmit={handleDurationChange}
                     />
                 </div>
                 
